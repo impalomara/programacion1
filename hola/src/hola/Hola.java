36b27ede26bfd00b86dfb6cc79cc378aa07edc86
impalomara <impalomara@gmail.com>
2024-12-17 16:13:45 +0100

    programacion eclipse

---
+++ hola/src/hola/Hola.java
@@ -0,0 +1,10 @@
+package hola;
+
+import java.util.Scanner;
+
+public class Hola {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("hola como estas");
+	}
+}
