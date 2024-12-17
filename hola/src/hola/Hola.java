c90cd71da6618d05474e669ddb133ff9bb0cbb5d
impalomara <impalomara@gmail.com>
2024-12-17 16:16:43 +0100

    segunda ronda

--- hola/src/hola/Hola.java
+++ hola/src/hola/Hola.java
@@ -6,5 +6,7 @@ public class Hola {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("hola como estas");
+		
+		System.out.println("adios");
 	}
 }
